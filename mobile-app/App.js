1a0b4b5615576de4391f6a1cfc15a0cf7e5eb1f5
Tyler Kirby <tyler.kirby9398@gmail.com>
2020-10-25 15:09:07 -0500

    added camera

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,12 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Camera style={{ flex: 1 }} type={type}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              justifyContent: 'center'
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 0.4,
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+                backgroundColor: 'white',
+                borderRadius: 50,
+                marginBottom: 40
+              }}
+              onPress={() => {
+                console.log('snapped photo')
+              }}>
+              <Text style={{ fontSize: 18, margin: 10, color: '#32CD32', fontWeight: 'bold' }}> Capture </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
     </View>
   );
 }
