02b6027ffdde2a85edd460f530271757b7a7c802
Tyler Kirby <tyler.kirby9398@gmail.com>
2020-11-14 11:57:07 -0600

    added new endpoints

--- analytics-server/main.py
+++ analytics-server/main.py
@@ -16,6 +16,18 @@ def get_temp(start: int = 0, end: int = 0):
         'temps': [np.sin(i)*3+50 for i in np.arange(0, 24, 1)]
     }
 
+@app.get('/soil/')
+def get_temp(start: int = 0, end: int = 0):
+    return {
+        'temps': [np.sin(i)*3+50 for i in np.arange(0, 24, 1)]
+    }
+
+@app.get('/light/')
+def get_temp(start: int = 0, end: int = 0):
+    return {
+        'temps': [np.sin(i)*3+50 for i in np.arange(0, 24, 1)]
+    }
+
 
 if __name__ == '__main__':
     uvicorn.run(app, port=5000)
